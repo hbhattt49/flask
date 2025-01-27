ce00cdf12c6d7c9dbca711cd90c4a59a2af88034
hbhattt49 <hbhatt49@gmail.com>
2025-01-27 20:22:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,30 @@
-
+# Route for User 1
 apiVersion: route.openshift.io/v1
 kind: Route
 metadata:
-  name: jupyterlab-route
+  name: jupyterlab-user1-route
 spec:
-  host: example.com  # Replace with your desired domain
-  path: /
-  rules:
-    - path: /user1
-      to:
-        kind: Service
-        name: jupyterlab-user1-service
-        weight: 100
-    - path: /user2
-      to:
-        kind: Service
-        name: jupyterlab-user2-service
-        weight: 100
+  host: example.com
+  path: /user1
+  to:
+    kind: Service
+    name: jupyterlab-user1-service
   tls:
-    termination: edge  # Use 'passthrough' if you want SSL termination at the backend
+    termination: edge
+    insecureEdgeTerminationPolicy: Redirect
+
+---
+# Route for User 2
+apiVersion: route.openshift.io/v1
+kind: Route
+metadata:
+  name: jupyterlab-user2-route
+spec:
+  host: example.com
+  path: /user2
+  to:
+    kind: Service
+    name: jupyterlab-user2-service
+  tls:
+    termination: edge
     insecureEdgeTerminationPolicy: Redirect

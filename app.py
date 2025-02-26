97b45be3a6e9d341bb328cf6e24dc0febbf081da
hbhattt49 <hbhatt49@gmail.com>
2025-02-26 15:19:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,6 @@
-apiVersion: apps/v1
-kind: Deployment
-metadata:
-  name: jupyterlab
-spec:
-  template:
-    spec:
-      containers:
-      - name: jupyterlab
-        image: jupyter/base-notebook
-        args:
-          - "jupyter"
-          - "lab"
-          - "--NotebookApp.allow_origin=https://web-ui.apps.openshift-cluster.com"
-          - "--NotebookApp.tornado_settings={\"headers\": {\"Content-Security-Policy\": \"frame-ancestors 'self' https://web-ui.apps.openshift-cluster.com;\"}}"
+export JUPYTER_ALLOW_ORIGIN="https://web-ui.apps.openshift-cluster.com"
+export JUPYTER_TORNADO_SETTINGS='{"headers": {"Content-Security-Policy": "frame-ancestors '\''self'\'' https://web-ui.apps.openshift-cluster.com;"}}'
+
+jupyter lab \
+  --NotebookApp.allow_origin="$JUPYTER_ALLOW_ORIGIN" \
+  --NotebookApp.tornado_settings="$JUPYTER_TORNADO_SETTINGS"

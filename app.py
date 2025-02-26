f20d9e2b7e8a00da844c2ea12e25ad20ce5611e0
hbhattt49 <hbhatt49@gmail.com>
2025-02-26 15:29:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,29 @@
-export JUPYTER_ALLOW_ORIGIN="https://web-ui.apps.openshift-cluster.com"
-export JUPYTER_TORNADO_SETTINGS='{"headers": {"Content-Security-Policy": "frame-ancestors '\''self'\'' https://web-ui.apps.openshift-cluster.com;"}}'
-
-jupyter lab \
-  --NotebookApp.allow_origin="$JUPYTER_ALLOW_ORIGIN" \
-  --NotebookApp.tornado_settings="$JUPYTER_TORNADO_SETTINGS"
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: jupyterlab
+spec:
+  replicas: 1
+  selector:
+    matchLabels:
+      app: jupyterlab
+  template:
+    metadata:
+      labels:
+        app: jupyterlab
+    spec:
+      containers:
+      - name: jupyterlab
+        image: jupyter/base-notebook  # Change this to your JupyterLab image
+        ports:
+          - containerPort: 8888
+        env:
+          - name: JUPYTER_ALLOW_ORIGIN
+            value: "https://web-ui.apps.openshift-cluster.com"
+          - name: JUPYTER_TORNADO_SETTINGS
+            value: '{"headers": {"Content-Security-Policy": "frame-ancestors '\''self'\'' https://web-ui.apps.openshift-cluster.com;"}}'
+        args:
+          - "jupyter"
+          - "lab"
+          - "--NotebookApp.allow_origin=$(JUPYTER_ALLOW_ORIGIN)"
+          - "--NotebookApp.tornado_settings=$(JUPYTER_TORNADO_SETTINGS)"

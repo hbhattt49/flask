786912eb10b703380d725117b47eaccc0d18aa1e
hbhattt49 <hbhatt49@gmail.com>
2025-05-07 13:10:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,14 @@
-from notebook.base.handlers import IPythonHandler
-from notebook.utils import url_path_join
-from tornado import web
-from notebook.notebookapp import list_running_servers
-import os
+from notebook.notebookapp import random_token
 
-used_tokens = set()
+# Set a fresh token at startup
+token = random_token()
+print(f"Generated one-time token: {token}")
 
-class OneTimeTokenHandler(IPythonHandler):
-    def prepare(self):
-        token = self.get_argument('token', default=None)
+c.ServerApp.token = token
+c.ServerApp.open_browser = False
+c.ServerApp.allow_origin = '*'
+c.ServerApp.server_extensions.append('one_time_token_extension')
 
-        if not token:
-            raise web.HTTPError(401, "Token required")
-
-        if token in used_tokens:
-            raise web.HTTPError(403, "Token has already been used")
-
-        # Mark token as used
-        used_tokens.add(token)
-
-        # (Optional) Regenerate token or log here
-        print(f"Token '{token}' has been used and is now invalid.")
-
-    def get(self):
-        self.finish("Access granted with one-time token.")
-
-def load_jupyter_server_extension(nbapp):
-    web_app = nbapp.web_app
-    host_pattern = ".*$"
-    route_pattern = url_path_join(web_app.settings["base_url"], "/one-time-auth")
-    web_app.add_handlers(host_pattern, [(route_pattern, OneTimeTokenHandler)])
-    nbapp.log.info("One-time token extension loaded.")
+# You can optionally store the token somewhere
+with open('/tmp/current_token.txt', 'w') as f:
+    f.write(token)

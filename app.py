b7711e08b17ad10200cc109b765d37865847dcb8
hbhattt49 <hbhatt49@gmail.com>
2025-05-07 14:40:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,41 +1,21 @@
-from jupyter_server.base.handlers import JupyterHandler
-from notebook.utils import url_path_join
-from tornado.web import HTTPError
 import secrets
+import sys
 
-# Token management
-used_tokens = set()
-new_token = secrets.token_urlsafe(24)
+# Include custom extension path
+sys.path.append("/home/YOUR_USERNAME/.jupyter/custom_extensions")
 
-def generate_token():
-    return secrets.token_urlsafe(24)
+initial_token = secrets.token_urlsafe(24)
+print(f"[Initial token]: {initial_token}")
 
-class OneTimeAuthHandler(JupyterHandler):
-    def prepare(self):
-        token = self.get_argument('token', None)
+c.ServerApp.token = initial_token
+c.ServerApp.open_browser = False
+c.ServerApp.allow_origin = '*'
 
-        if not token:
-            raise HTTPError(401, "Token missing")
+# Register extension
+c.ServerApp.jpserver_extensions = {
+    "one_time_token_extension": True
+}
 
-        if token in used_tokens:
-            raise HTTPError(403, "This token has already been used.")
-
-        # Allow access and expire token
-        used_tokens.add(token)
-        global new_token
-        new_token = generate_token()
-
-        # Save new token for next time
-        with open('/tmp/current_token.txt', 'w') as f:
-            f.write(new_token)
-
-        print(f"[Token used]: {token}")
-        print(f"[New token generated]: {new_token}")
-
-    def get(self):
-        self.finish("Access granted. This token is now expired.")
-
-def load_jupyter_server_extension(server_app):
-    route_pattern = url_path_join(server_app.web_app.settings["base_url"], "/one-time-auth")
-    server_app.web_app.add_handlers(".*$", [(route_pattern, OneTimeAuthHandler)])
-    server_app.log.info("One-time token extension loaded.")
+# Save initial token to temp file
+with open('/tmp/current_token.txt', 'w') as f:
+    f.write(initial_token)

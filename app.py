a163f564606cc642ef5368d1199438a477f96f35
hbhattt49 <hbhatt49@gmail.com>
2025-05-07 15:01:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,11 @@
-import secrets
-import sys
+from setuptools import setup, find_packages
 
-# Include custom extension path
-sys.path.append("/home/YOUR_USERNAME/.jupyter/custom_extensions")
-
-initial_token = secrets.token_urlsafe(24)
-print(f"[Initial token]: {initial_token}")
-
-c.ServerApp.token = initial_token
-c.ServerApp.open_browser = False
-c.ServerApp.allow_origin = '*'
-
-# Register extension
-c.ServerApp.jpserver_extensions = {
-    "one_time_token_extension": True
-}
-
-# Save initial token to temp file
-with open('/tmp/current_token.txt', 'w') as f:
-    f.write(initial_token)
+setup(
+    name='one_time_token_extension',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=['jupyter_server'],
+    entry_points={
+        'jupyter_serverproxy_servers': []
+    }
+)

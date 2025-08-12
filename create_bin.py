e7742b6d5943f70d7e07ab5b8872f5f94b9da6c2
hbhattt49 <hbhatt49@gmail.com>
2025-08-12 20:51:26 +0530

    Update create_bin.py

--- create_bin.py
+++ create_bin.py
@@ -3,3 +3,16 @@ tok = AutoTokenizer.from_pretrained("~/label-studio/model/BERT")
 m = TFAutoModelForSequenceClassification.from_pretrained("~/label-studio/model/BERT", from_tf=True)
 x = tok("sample text", return_tensors="tf")
 print(m(**x).logits.numpy())
+
+
+
+
+<View>
+  <Header value="Text Classification"/>
+  <Choices name="label" toName="text" choice="single" showInLine="true">
+    <Choice value="Negative"/>
+    <Choice value="Neutral"/>
+    <Choice value="Positive"/>
+  </Choices>
+  <Text name="text" value="$text"/>
+</View>
